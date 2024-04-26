6cb3e8622457d801206e2bf382922baf35375a42
beloved <toshiroakari@gmail.com>
2024-04-26 19:44:51 +0100

    Added autorun db to initialize function

--- rekai_initialize.py
+++ rekai_initialize.py
@@ -1,12 +1,14 @@
 from appconfig import AppConfig
-from db_management import SystemDBM, JishoParseDBM, TextToSpeechDBM, DeepLDBM, GoogleTLDBM, GeminiGPTDBM
+from db_management import SystemDBM, JishoParseDBM, TextToSpeechDBM, DeepLDBM, GoogleTLDBM, GeminiGPTDBM, AutorunDBM
 
 def rekai_initialize():
 
     sys_dbm = SystemDBM(mode=0)
     sys_dbm.check_and_initialize()
 
-    database_managers = [JishoParseDBM, TextToSpeechDBM, DeepLDBM, GoogleTLDBM, GeminiGPTDBM]
+    database_managers = [JishoParseDBM, TextToSpeechDBM, DeepLDBM, GoogleTLDBM, GeminiGPTDBM, AutorunDBM]
     for dbm in database_managers:
-        dbm = dbm()
+        dbm = dbm(mode=2)
         dbm.check_and_initialize()
+
+rekai_initialize()
